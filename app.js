50d3aa21e2f434806a376462a7d9ff1c2681bfbd
Yu Cheng Li <ychengli11065@gmail.com>
2023-07-18 17:04:21 -0700

    creating controller and route for login page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const mainRouter = require("./routes/main");
+const mainRouter = require('./routes/main');
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/main', mainRouter);
+app.use('/main', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,10 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const login_Controller = require('../controllers/loginController');
+
+
+router.get('/', login_Controller.login_get);
+
+module.exports = router;
 
