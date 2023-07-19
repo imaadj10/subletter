fc15206203ff25b538c04fc0f527ee54d1172a8b
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-18 22:46:17 -0700

    implemented login POST without the render

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,27 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const users = require('../tempData/data').userDB;
 
 exports.login_get = asyncHandler(async (req, res, next) => {
   res.send('Not implemented yet login-GET');
 });
 
 exports.login_post = asyncHandler(async (req, res, next) => {
+	const currUser = users.find((data) => req.body.email === data.email);
+
+	if (currUser) {
+		const inputPass = req.body.password;
+		const storedPass = currUser.password;
+
+		const passwordMatch = await bcrypt.compare(inputPass, storedPass);
+		if (passwordMatch) {
+			// Success Render page after login in
+		} else {
+			// Failed Wrong password Render same login page but with failed message
+		}
+	} else {
+		// Failed Invalid user Render same login page but with failed message
+	}
   res.send('Not implemented yet login-POST');
 });
 
