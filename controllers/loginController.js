5ba078a977a518a147cf043f500f57541e711dc6
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-18 23:41:08 -0700

    added create account feature

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,10 +4,14 @@ const users = require('../tempData/data').userDB;
 
 exports.login_get = asyncHandler(async (req, res, next) => {
 	// Render login page
-  res.send('Not implemented yet login-GET');
+	res.render("login.pug", {
+		title: "Login",
+	});
+  // res.send('Not implemented yet login-GET');
 });
 
 exports.login_post = asyncHandler(async (req, res, next) => {
+	res.redirect("/main/test");
 	const currUser = users.find((data) => req.body.email === data.email);
 
 	if (currUser) {
@@ -23,10 +27,14 @@ exports.login_post = asyncHandler(async (req, res, next) => {
 	} else {
 		// Failed Invalid user Render same login page but with failed message
 	}
-  res.send('Not implemented yet login-POST');
 });
 
-exports.register = asyncHandler(async (req, res, next) => {
+exports.register_get = asyncHandler(async (req, res, next) => {
+	res.send('Registration get page');
+});
+
+exports.register_post = asyncHandler(async (req, res, next) => {
+	res.send('Registration post page');
 	const foundUser = user.find((data) => req.body.email === data.email);
 
 	if (!foundUser) {
