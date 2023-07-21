eabcb2c500afc3607b0fbd97d574697db4c2d70f
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-20 17:29:44 -0700

    created all file paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import Homepage from './components/Homepage'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import Notifications from './components/Notifications';
+import Listings from './components/Listing';
+import Profile from './components/Profile';
+import HousingInfo from './components/HousingInfo';
+import MessageBoard from './components/MessageBoard';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={ <Login /> } exact/>
-        <Route path='/signup' element={ <Signup /> } />
-        <Route path='/home' element={ <Homepage /> } />
+        <Route path="/" element={<Login />} exact />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/housinginfo" element={<HousingInfo />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/messageboard" element={<MessageBoard />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/components/Notifications.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Notifications = () => {
+
+    return (
+        <div>
+            <h1> Welcome to Notifications!</h1>
+
+        </div>
+    );
+};
+
+export default Notifications;
