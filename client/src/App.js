9fe4df80d9ff7ca24feced74a3a104a32feb2aad
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-20 17:47:32 -0700

    finished up basis for routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Navbar from './components/Navbar';
 import Notifications from './components/Notifications';
-import Listings from './components/Listing';
+import Listings from './components/Listings';
 import Profile from './components/Profile';
 import HousingInfo from './components/HousingInfo';
 import MessageBoard from './components/MessageBoard';
