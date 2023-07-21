37878ec6aa5a36ce3f8b68840f0fa47b0ada3a4e
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-20 17:11:39 -0700

    set pages temp to work on routes

---
+++ client/src/components/MessageBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MessageBoard = () => {
+
+    return (
+        <div>
+            <h1> Welcome to MessageBoard!</h1>
+
+        </div>
+    );
+};
+
+export default MessageBoard;
