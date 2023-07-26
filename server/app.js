9efdf2a6e312221e4132fb3a99766cc85734612c
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-07-25 17:36:16 -0700

    removed sessions

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
