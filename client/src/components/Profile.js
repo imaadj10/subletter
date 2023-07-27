82ad6aa258002d871e144b03170e6db12160c813
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-26 20:54:19 -0700

    creating the logout function

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,19 @@
+import './Profile.css';
 import React from 'react';
 
+
 const Profile = () => {
+
+
+    const logout = () => {
+
+    }
 
     return (
         <div>
             <h1> Welcome to Profile!</h1>
+
+            <button className='logout-button' onClick={logout}>Logout</button>
 
         </div>
     );
