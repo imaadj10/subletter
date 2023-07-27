2ad820a0b7adb1d65205e2ee5202e2ba2ffe9678
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-07-26 18:09:48 -0700

    finished basis layout for item listings

--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -1,6 +1,7 @@
 import './Listings.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import image from '../assets/Orchard-Commons.jpg';
 
 const Listings = () => {
   const [listings, setListings] = useState([]);
@@ -18,19 +19,19 @@ const Listings = () => {
 
   return (
     <div>
-      <h1> Welcome to Listings!</h1>
-      <h1>Listings: </h1>
       <div className="listings">
         {listings.map((listing) => {
           return (
             <div className="listing" key={listing.id}>
-              <p className="listing-name">{listing.name}</p>
-              <p className="listing-residence">{listing.residence}</p>
-              <p className="listing-location">{listing.location}</p>
-              <p className="listing-price">{listing.price}</p>
+              <div className="listing-image">
+                <img src={image} alt="Tallwood"/>
+              </div>
+              <div className="listing-name">{listing.name}</div>
+              <div className="listing-residence">{listing.residence}</div>
+              <div className="listing-location">{listing.location}</div>
+              <div className="listing-price">{listing.price}</div>
             </div>
           );
-          // return  <li key={listing.id}>{listing.name}</li>;
         })}
       </div>
     </div>
