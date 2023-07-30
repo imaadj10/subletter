26053249eb22647654b6dc508697242beed3cc6d
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-07-29 19:19:06 -0700

    Removed deprecated db connection

--- server/mysql/mysql.js
+++ server/mysql/mysql.js
@@ -1,35 +1,12 @@
-// const mysql = require('mysql2');
-
-// require('dotenv').config();
-
-// const db = mysql.createConnection({
-//   host: process.env.DATABASE_IP_ADDRESS,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DATABASE,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to the database:', err);
-//     return;
-//   }
-//   console.log('Connected to the database!');
-// });
-
-// module.exports = db;
-
-const mysql = require('mysql2/promise'); // Import the promise-based version
+const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 
-const pool = mysql.createPool({ // Use createPool instead of createConnection
+const pool = mysql.createPool({
   host: process.env.DATABASE_IP_ADDRESS,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
 });
 
-// The connection callback is not needed with createPool
-
-module.exports = pool; // Export the pool instead of the connection object
+module.exports = pool; 
