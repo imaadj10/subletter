5115ebe92565060d0e53b7162a446271e0e03dd7
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-08-01 20:35:45 -0700

    notification is working with  minor bugs

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -3,15 +3,21 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import SingleNotification from './SingleNotification';
 import '../css/Notifications.css';
+import Cookies from 'universal-cookie';
+
 
 const Notifications = () => {
   const [notifications, setNotifications] = useState([]);
+  const cookies = new Cookies();
+  const token = cookies.get('TOKEN');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         await axios
-          .get('http://localhost:1234/notifications')
+          .get('http://localhost:1234/notifications', {
+            headers: { Authorization: `Bearer ${token}` },
+          })
           .then((res) => {
             const curr_notif = {
               id: res.data.nid,
@@ -28,7 +34,7 @@ const Notifications = () => {
     };
 
     fetchData();
-  }, [notifications]);
+  }, []);
 
   return (
     <div className="notifs">
