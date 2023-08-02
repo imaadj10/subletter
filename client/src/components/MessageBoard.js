4ca4910730be846064972daba33997ab5ac45365
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-08-01 19:47:19 -0700

    added GET messages functionality

--- client/src/components/MessageBoard.js
+++ client/src/components/MessageBoard.js
@@ -1,44 +1,63 @@
 import '../css/Messages.css';
-import { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import { useState, useEffect } from 'react';
 const sendIcon = require('../assets/send.png');
 
 const MessageBoard = () => {
-  const [users, setUsers] = useState([
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-  ]);
+  const cookies = new Cookies();
+  const token = cookies.get('TOKEN');
+  const username = cookies.get('USERNAME');
+  const [conversations, setConversations] = useState([]);
+  const [conversation_partner, setPartner] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:1234/messages`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setConversations(res.data);
+      })
+      .catch((e) => {
+        console.log('Error fetching listings data');
+      });
+  }, []);
+
+  const handleConversationClick = (conversation_partner) => {
+    setPartner(conversation_partner);
+    axios
+      .get(`http://localhost:1234/messages/${conversation_partner}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setMessages(res.data);
+      })
+      .catch((e) => {
+        console.log('Error fetching listings data');
+      });
+  }
 
   return (
     <div>
       <div className="chat-message-container">
-        <div className="users">
-          <h1 className="chat-h1">Chat Messages</h1>
-          {users.map((user) => {
-            return <div className="user">{user}</div>;
+        <div className="conversations">
+          <h1 className="chat-h1">Conversations</h1>
+          {conversations.map((conversation) => {
+            return <div className="conversation"
+                    onClick={() => handleConversationClick(conversation.conversation_partner)}>{conversation.conversation_partner}</div>;
           })}
         </div>
         <div className="messages-container">
-          <h2 className="name-header">Yu Cheng Li</h2>
+          <h2 className="name-header">{conversation_partner}</h2>
           <div className="chat-box">
-            {users.map((user) => {
-              return <div className="message-left">{user}</div>;
-            })}
-            {users.map((user) => {
-              return <div className="message-right">{user}</div>;
+            {messages.map((message) => {
+              return message.sid === username ? (
+                <div className="message-right">{message.content}</div>
+              ) : (
+                <div className="message-left">{message.content}</div>
+              );
             })}
           </div>
           <form className="text-bar">
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const messages_controller = require('../controllers/messages_controller');
 const { isAuthenticated } = require('../middleware/auth_middleware');
 
-router.get('/', isAuthenticated, messages_controller.get_user_messages);
+router.get('/', isAuthenticated, messages_controller.get_conversations);
+
+router.get('/:conversation', isAuthenticated, messages_controller.get_conversation_messages);
 
 module.exports = router
