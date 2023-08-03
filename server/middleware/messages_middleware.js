e6763a22ff16ba74cb643b6abdd4c9ae219db750
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-08-03 00:30:32 -0700

    removed variable names from messages query

--- server/middleware/messages_middleware.js
+++ server/middleware/messages_middleware.js
@@ -6,39 +6,43 @@ exports.get_conversations = async (req) => {
                     FROM (
                         SELECT 
                             CASE
-                                WHEN sid = '${req.user.username}' THEN rid
+                                WHEN sid = ? THEN rid
                                 ELSE sid
                             END AS conversation_partner,
                             MAX(time_sent) AS max_time_sent
                         FROM
                             messages
                         WHERE
-                            '${req.user.username}' IN (sid, rid)
+                            ? IN (sid, rid)
                         GROUP BY
                             conversation_partner
                     ) AS subquery
                     ORDER BY
                         max_time_sent DESC`;
-    const result = await db.query(query);
+    const result = await db.query(query, [req.user.username, req.user.username]);
     return result[0];
 };
 
 exports.get_conversation_messages = async (req) => {
-    const query = req.user.username !== req.params.conversation ?
-                  `SELECT * FROM messages
-                    WHERE (sid = '${req.user.username}' OR rid = '${req.user.username}') AND (sid = '${req.params.conversation}' OR rid = '${req.params.conversation}')
-                    ORDER BY time_sent ASC;` :
-                  `SELECT * FROM messages
-                    WHERE sid = '${req.user.username}' AND rid = '${req.user.username}'
-                    ORDER BY time_sent ASC;`
-    const result = await db.query(query);
-    return result[0];
+    if (req.user.username !== req.params.conversation) {
+        const query = `SELECT * FROM messages
+                        WHERE (sid = ? OR rid = ?) AND (sid = ? OR rid = ?)
+                        ORDER BY time_sent ASC;`;
+        const result = await db.query(query, [req.user.username, req.user.username, req.params.conversation, req.params.conversation]);
+        return result[0];
+    } else {
+        const query = `SELECT * FROM messages
+                        WHERE sid = ? AND rid = ?
+                        ORDER BY time_sent ASC;`
+        const result = await db.query(query, [req.user.username, req.user.username]);
+        return result[0];
+    }
 };
 
 exports.send_new_message = async (req) => {
     const query = `INSERT INTO messages(sid, rid, content)
-                    VALUES ('${req.user.username}', '${req.params.conversation}', '${req.body.message}')`;
-    await db.query(query);
+                    VALUES (?, ?, ?)`;
+    await db.query(query, [req.user.username, req.params.conversation, req.body.message]);
 
     // Emit the 'new_message' event to relevant clients using the getIO function
     const io = getIO();
