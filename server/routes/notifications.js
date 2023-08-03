e844c74d10a66dc62e42a245de9f9fbfc6b813e3
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-08-03 00:33:37 -0700

    cleaned up remaining routes

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const notification_controller = require('../controllers/notification_controller')
 const { isAuthenticated } = require('../middleware/auth_middleware');
 
-
 router.get('/', isAuthenticated, notification_controller.retrieve_notifications);
 
 router.post('/', notification_controller.add_notification);
--- server/routes/schools.js
+++ server/routes/schools.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllSchools } = require('../controllers/schools_controller');
 
-router.route('/').get(getAllSchools);
+router.get('/', getAllSchools);
 
 module.exports = router;
