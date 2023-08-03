c7a1bd972a10a21b885e6bca6593630e3d557268
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-08-03 00:59:48 -0700

    added frontend changes to delete notification

--- client/src/components/SingleNotification.js
+++ client/src/components/SingleNotification.js
@@ -1,14 +1,31 @@
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
 export default function SingleNotification(props) {
   const { id, title, content, setNotifications } = props;
+  const cookies = new Cookies();
+  const token = cookies.get('TOKEN');
 
-  const deleteNotification = () => {
-    // deleteNotificationDB()
-    // setNotifications((prevNotifs) => {
-    //   prevNotifs.filter((notif) => {
-    //     return notif.id !== id;
-    //   });
-    // });
-  };
+  async function deleteNotification() {
+    try {
+      await axios
+        .delete('http://localhost:1234/notifications', {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          setNotifications((prevNotifs) => {
+            prevNotifs.filter((notif) => {
+              return notif.id !== id;
+            });
+          });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <li className="notif-item">
