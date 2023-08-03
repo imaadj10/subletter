339422b76e1fd3a638673f6c5955b18b520d9059
Yu Cheng Li <ychengl1@students.cs.ubc.ca>
2023-08-02 20:55:59 -0700

    notification login works

--- server/controllers/notification_controller.js
+++ server/controllers/notification_controller.js
@@ -8,3 +8,14 @@ exports.retrieve_notifications = async (req, res) => {
         res.status(401).json({ message: 'Error Fetching Notifications' });
     }
 };
+
+exports.add_notification = async (req, res) => {
+    try {
+        console.log(req.body);
+        await notification_middleware.add_notification(req);
+        res.status(201).send('Added notification!');
+    } catch(e) {
+        console.log(e);
+        res.status(400).json({ message: 'Unable to add notification'});
+    }
+};
