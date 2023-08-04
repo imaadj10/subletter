8b1752ca4017de94e1a93fdd0cc63af5582e8ddf
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-08-03 17:19:42 -0700

    Fixed get single residence

--- server/middleware/housing_middleware.js
+++ server/middleware/housing_middleware.js
@@ -12,9 +12,10 @@ exports.getUserHousing = async (req) => {
 };
 
 exports.get_residence = async (req) => {
-  const query = `SELECT * FROM residences r
+  const query = `SELECT r.res_name, r.school_name, r.street_address, r.postal_code, r.country, a1.city, a1.province, c.type, c.price FROM residences r
                   INNER JOIN addresses_1 a1 ON r.postal_code = a1.postal_code AND r.country = a1.country
-                  WHERE res_name = ? AND school_name = ?`;
+                  INNER JOIN contains c ON r.res_name = c.res_name AND r.school_name = c.school_name
+                  WHERE r.res_name = ? AND r.school_name = ?`;
   const result = await db.query(query, [req.params.residence, req.user.school]);
   return result[0];
 };
