ba7953ae6c6c2268cd9221ded5093699067984f2
Andy Hu <ahu04@students.cs.ubc.ca>
2023-08-04 21:26:18 -0700

    add try catch to review middleware

--- server/middleware/reviews_middleware.js
+++ server/middleware/reviews_middleware.js
@@ -1,45 +1,61 @@
 const db = require('../mysql/mysql');
 
-exports.getListingComments = async (req) => {
-  const id = parseInt(req.params.id);
-};
-
 exports.getReviews = async (req) => {
-  const query = `SELECT rid, username, description, rating FROM reviews WHERE res_name=? AND school_name=?`;
-  const queryResult = await db.query(query, [
-    req.params.residence,
-    req.user.school,
-  ]);
-  return queryResult;
+  try {
+    const query = `SELECT rid, username, description, rating FROM reviews WHERE res_name=? AND school_name=?`;
+    const queryResult = await db.query(query, [
+      req.params.residence,
+      req.user.school,
+    ]);
+    return queryResult;
+  } catch (e) {
+    throw e;
+  }
 };
 
 exports.getRating = async (req) => {
-  const query = `SELECT AVG(rating) AS overall FROM reviews WHERE res_name=? AND school_name=?`;
-  const queryResult = await db.query(query, [
-    req.params.residence,
-    req.user.school,
-  ]);
-  return queryResult;
+  try {
+    const query = `SELECT AVG(rating) AS overall FROM reviews WHERE res_name=? AND school_name=?`;
+    const queryResult = await db.query(query, [
+      req.params.residence,
+      req.user.school,
+    ]);
+    return queryResult;
+  } catch (e) {
+    throw e;
+  }
 };
 
 exports.getAllResidenceReviews = async (req) => {
-  const query = `SELECT res_name, AVG(rating) AS overall FROM reviews WHERE school_name=? GROUP BY res_name HAVING COUNT(*) > 1`;
-  const queryResult = await db.query(query, [req.user.school]);
-  return queryResult;
+  try {
+    const query = `SELECT res_name, AVG(rating) AS overall FROM reviews WHERE school_name=? GROUP BY res_name HAVING COUNT(*) > 1`;
+    const queryResult = await db.query(query, [req.user.school]);
+    return queryResult;
+  } catch (e) {
+    throw e;
+  }
 };
 
 exports.createResidenceReview = async (req) => {
-  const query = `INSERT INTO reviews(username, description, res_name, school_name, rating) VALUES (?, ?, ?, ?, ?)`;
-  await db.query(query, [
-    req.user.username,
-    req.body.content,
-    req.params.residence,
-    req.user.school,
-    req.body.rating,
-  ]);
+  try {
+    const query = `INSERT INTO reviews(username, description, res_name, school_name, rating) VALUES (?, ?, ?, ?, ?)`;
+    await db.query(query, [
+      req.user.username,
+      req.body.content,
+      req.params.residence,
+      req.user.school,
+      req.body.rating,
+    ]);
+  } catch (e) {
+    throw e;
+  }
 };
 
 exports.deleteReview = async (req) => {
-  const query = `DELETE FROM reviews WHERE rid=?`;
-  await db.query(query, [req.params.residence]); // residence is the review id here, really weird
+  try {
+    const query = `DELETE FROM reviews WHERE rid=?`;
+    await db.query(query, [req.params.residence]); // residence is the review id here, really weird
+  } catch (e) {
+    throw e;
+  }
 };
