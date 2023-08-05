d50811447dbf257022ebabced1c4a5821a9c2e1b
Imaad Junaidi <imaadj10@students.cs.ubc.ca>
2023-08-05 13:35:42 -0700

    Moved where listings saves images

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: './images' });
+const upload = multer({ dest: './images/listings' });
 const router = express.Router();
 const listings_controller = require('../controllers/listings_controller');
 const { isAuthenticated } = require('../middleware/auth_middleware');
